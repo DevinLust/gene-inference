fa1f732aac7db77ee7d3a95f2f7ea78202cb7041
Devin Lust <lust.65@osu.edu>
2025-10-13 19:23:30 -0400

    Add values to grades for ranking

--- backend/src/main/java/com/progressengine/geneinference/model/enums/Grade.java
+++ backend/src/main/java/com/progressengine/geneinference/model/enums/Grade.java
@@ -1,7 +1,16 @@
 package com.progressengine.geneinference.model.enums;
 
 public enum Grade {
-    S, A, B, C, D, E;
+    S(5), A(4), B(3), C(2), D(1), E(0);
+
+    private final int value;
+    Grade(int value) {
+        this.value = value;
+    }
+
+    public boolean isBetterThan(Grade other) {
+        return this.value > other.value;
+    }
 
     public static Grade fromString(String str) {
         return Grade.valueOf(str); // Throws IllegalArgumentException if not valid
