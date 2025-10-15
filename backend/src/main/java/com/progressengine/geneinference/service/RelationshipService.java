a63ccfe5e05a989ad5fe7eda54e15f0e254841ed
Devin Lust <lust.65@osu.edu>
2025-10-14 20:39:09 -0400

    Add validation and error messages to Relationship services

--- backend/src/main/java/com/progressengine/geneinference/service/RelationshipService.java
+++ backend/src/main/java/com/progressengine/geneinference/service/RelationshipService.java
@@ -38,13 +38,11 @@ public class RelationshipService {
      * Finds or creates a Relationship for the two sheep.
      */
     public Relationship findOrCreateRelationship(Sheep sheep1, Sheep sheep2) {
+        relationshipValidation(sheep1, sheep2); // validates these two sheep can be in relationship
+
         // Determine the lower and higher sheep IDs
         Integer sheepId1 = sheep1.getId();
         Integer sheepId2 = sheep2.getId();
-
-        if (sheep1 == sheep2 || sheepId1.equals(sheepId2)) {
-            throw new IllegalArgumentException("Cannot breed a sheep with itself!");
-        }
 
         Integer parent1Id = Math.min(sheepId1, sheepId2);
         Sheep parent1 = sheepId1 < sheepId2 ? sheep1 : sheep2;
@@ -66,7 +64,7 @@ public class RelationshipService {
 
         // set other fields as needed
 
-        return relationshipRepository.save(newRelationship);
+        return newRelationship;
     }
 
     public List<Relationship> findRelationshipsByParent(Sheep parent) {
@@ -101,6 +99,8 @@ public class RelationshipService {
 
     // assumes uniform distribution for child
     public static Sheep breedNewSheep(Relationship relationship) {
+        breedingValidation(relationship);
+
         Sheep child = new Sheep();
 
         Sheep parent1 = relationship.getParent1();
@@ -138,5 +138,33 @@ public class RelationshipService {
 
     public RelationshipResponseDTO toResponseDTO(Relationship relationship) {
         return new RelationshipResponseDTO(relationship);
+    }
+
+    // validates that these two sheep can be in a relationship
+    private static void relationshipValidation(Sheep sheep1, Sheep sheep2) {
+        if (sheep1 == sheep2 || sheep1.getId().equals(sheep2.getId())) {
+            throw new IllegalArgumentException("Cannot breed a sheep with itself!");
+        }
+    }
+
+    // validates that these two sheep can be automatically bred within the app
+    private static void breedingValidation(Relationship relationship) {
+        Set<Category> parent1Missing = missingHiddenAllele(relationship.getParent1());
+        Set<Category> parent2Missing = missingHiddenAllele(relationship.getParent2());
+        if (!parent1Missing.isEmpty() || !parent2Missing.isEmpty()) {
+            String parent1MissingStr = parent1Missing.isEmpty() ? "" : String.format("Parent 1 is missing hidden alleles in: %s%n", parent1Missing);
+            String parent2MissingStr = parent2Missing.isEmpty() ? "" : String.format("Parent 2 is missing hidden alleles in: %s%n", parent2Missing);
+            throw new IllegalArgumentException(String.format("Cannot breed sheep with missing hidden alleles!%n%s%s", parent1MissingStr, parent2MissingStr));
+        }
+    }
+
+    private static Set<Category> missingHiddenAllele(Sheep sheep) {
+        Set<Category> categories = EnumSet.noneOf(Category.class);
+        for (Category category : Category.values()) {
+            if (sheep.getHiddenAllele(category) == null) {
+                categories.add(category);
+            }
+        }
+        return categories;
     }
 }
