995857e104d9bc3bc6bda990d963c1c4216aca71
Devin Lust <lust.65@osu.edu>
2026-03-06 14:20:37 -0500

    Change security config for CI

--- backend/src/main/java/com/progressengine/geneinference/config/JwtValidationConfig.java
+++ backend/src/main/java/com/progressengine/geneinference/config/JwtValidationConfig.java
@@ -3,12 +3,14 @@ package com.progressengine.geneinference.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.core.*;
 import org.springframework.security.oauth2.jwt.*;
 
 import java.util.List;
 
 @Configuration
+@Profile("!ci")
 public class JwtValidationConfig {
 
     @Bean
